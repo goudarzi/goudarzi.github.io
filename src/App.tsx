dfd49f42121899477215da07e12dacc8586cd4e6
Rikki Goudarzi <10061151+goudarzi@users.noreply.github.com>
2021-04-24 16:21:41 +1200

    Updated default message

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Winter is coming...
         </a>
       </header>
     </div>
