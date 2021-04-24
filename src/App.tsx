abe39d2311f9bafea52aa6a498f49a98c75fffc6
Rikki Goudarzi <10061151+goudarzi@users.noreply.github.com>
2021-04-24 16:34:18 +1200

    Updated docs

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Winter is coming...
-        </a>
+        Winter is coming...
       </header>
     </div>
   );
